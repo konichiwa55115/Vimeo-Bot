783dda7ecf4fb15bea150cded482db863ee958c8
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-07 14:46:46 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6089522865:AAG0vUsYSBg-BaSfiG1T5E1QTrorjoMDA4U")
 
-    API_ID = int(os.environ.get("API_ID", 6))
+    API_ID = int(os.environ.get("API_ID", 17983098))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "ee28199396e0925f1f44d945ac174f64")
